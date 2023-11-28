efb717091d7e989940a1483ee3856f159d83d8b7
efa07 <efatariku07@gmmail.com>
2023-11-28 19:18:19 +0300

    interview

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+/**
+ * insert_node - Insert node in order to linkedlist
+ * @head: head of linkedlist
+ * @number: number to be added
+ * Return: the address of new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *actual = *head;
+
+	if (!new)
+		return (NULL);
+
+	new->n = number;
+
+	if (*head == NULL || (*head)->n > number)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
+	while (actual->next)
+	{
+		if ((actual->next)->n >= number)
+		{
+			new->next = actual->next;
+			actual->next = new;
+			return (new);
+		}
+		actual = actual->next;
+	}
+
+	new->next = NULL;
+	actual->next = new;
+
+	return (new);
+}
