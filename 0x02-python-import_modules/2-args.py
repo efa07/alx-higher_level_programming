c8169cd903dd869c89ef66fae588a7d615cf3e40
efa07 <efatariku07@gmmail.com>
2023-11-30 19:19:22 +0300

    argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg_len = len(sys.argv)
+    if arg_len == 1:
+        print("{} arguments.".format(arg_len - 1))
+    elif arg_len == 2:
+        print("{} argument:".format(arg_len - 1))
+    else:
+        print("{} arguments:".format(arg_len - 1))
+
+    for i in range(1, arg_len):
+        print("{}: {}".format(i, sys.argv[i]))
