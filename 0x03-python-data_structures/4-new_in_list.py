ef9e5822ee974497ef74cbc691803352e4851da1
efa07 <efatariku07@gmmail.com>
2023-12-02 15:48:48 +0300

    add new lis

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list
     lis_len = len(my_list)
+    my_lis_copy = my_list.copy()
     if idx < 0 or idx >= lis_len:
-        return new_list
+        return my_lis_copy
     else:
-        new_list[idx] = element
-        return new_list
-    
+        my_lis_copy[idx] = element
+        return my_lis_copy   
