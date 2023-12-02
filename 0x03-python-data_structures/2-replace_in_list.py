c4f16cefabc07940a69fcd5cbc05fe79e0d01463
efa07 <efatariku07@gmmail.com>
2023-12-02 15:00:52 +0300

    add new ele

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    new_list = []
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    elif:
+        new_list = my_list
+        new_list[idx] = new_element
+
