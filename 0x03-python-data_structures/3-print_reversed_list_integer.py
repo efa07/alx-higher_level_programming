423e7b092ce42db0d2b774e2dca42b75761d51c4
efa07 <efatariku07@gmmail.com>
2023-12-02 15:30:17 +0300

    reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if 1 == 1:
+    my_list.revers()
+    lis_len = len(my_list)
+    for i in range(lis_len):
+        print("{:d}".format(my_list[i]))
