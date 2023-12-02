5fb7e2bad2be9d33ad3d8a814b4b836b7c839ab3
efa07 <efatariku07@gmmail.com>
2023-12-02 21:20:51 +0300

    mul return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    my_tuple = ()
     length = len(sentence)
+    my_tuple = ()
     if length == 0:
         my_tuple = 0, "None"
     else:
         my_tuple = length, sentence[0]
-        return my_tuple
+    return my_tuple
