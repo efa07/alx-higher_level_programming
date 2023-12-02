2631f38ac05f3e61c0ab61885f8fc903c000ac8c
efa07 <efatariku07@gmmail.com>
2023-12-02 15:07:15 +0300

    add new ele

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    new_list = []
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        new_list = my_list
-        new_list[idx] = new_element
-        return new_list
+        my_list[idx] = new_element
+        return my_list
 
