05afc28d824cb00f3494430d059ef0eb9399277d
Efa <efatariku07@gmail.com>
2023-12-18 21:30:06 +0300

    fix-bug

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
+
 def safe_print_list_integers(my_list=[], x=0):
-
-
     num = 0
     for i in range(0, x):
         try:
@@ -10,5 +9,5 @@ def safe_print_list_integers(my_list=[], x=0):
             num += 1
         except (ValueError, TypeError):
             continue
-    print("")
-    return num
+        print("")
+        return num
