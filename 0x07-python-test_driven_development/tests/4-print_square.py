138eeed7cdebf7c64be65241a2e2ca8444555023
Efa <efatariku07@gmail.com>
2024-01-07 11:29:04 +0300

    print #

---
+++ 0x07-python-test_driven_development/tests/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Prints a square with the character #.
+
+"""
+
+
+def print_square(size):
+    """
+    Prints a square where size is
+    the length and width of the square.
+
+    """
+
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
