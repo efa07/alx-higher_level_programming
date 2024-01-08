0973e8129e436d61a00524650e0340f078e42387
Efa <efatariku07@gmail.com>
2024-01-08 19:25:36 +0300

    gemotry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+    def area(self):
+        """Raises an Exception error """
+        raise Exception("area() is not implemented")
