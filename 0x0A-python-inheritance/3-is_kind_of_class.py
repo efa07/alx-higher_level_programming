dfa779584402b1b561d40cb22f31f5b1b6a6099a
Efa <efatariku07@gmail.com>
2024-01-08 19:20:12 +0300

    cheack class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns:
+        True: if the object is an instance of a class
+        False: otherwise
+        """
+    return isinstance(obj, a_class)
