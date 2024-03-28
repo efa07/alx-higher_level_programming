12ab1e84148cd166b4783aeab3f605762caf38d8
efa07 <efatariku07@gmail.com>
2024-03-28 21:22:57 +0300

    finds a peak in a list of unsorted integers

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module that finds a peak in a list of integers"""
+
+
+def find_peak(list_of_integers):
+    """function that finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers (list): a list of integers
+
+    Returns:
+        int: peak(s)
+    """
+    list_ = list_of_integers
+
+    if list_ == []:
+        return None
+    length = len(list_)
+
+    start, end = 0, length - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
+            return list_[mid]
+        if list_[mid - 1] > list_[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return list_[start]
