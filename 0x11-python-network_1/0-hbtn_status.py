e36c30ff9d969a6d81fecd7778582c1c250446c4
efa07 <efatariku07@gmail.com>
2024-03-30 09:18:23 +0300

    fetches url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Fetches data from
+https://alx-intranet.hbtn.io/status
+"""
+
+
+import urllib.request
+
+with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode('utf-8')))
