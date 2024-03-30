b4d4338dc8ddef7780eb9044d78543b4a7b99862
efa07 <efatariku07@gmail.com>
2024-03-30 10:44:30 +0300

    print text

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ fetches https://intranet.hbtn.io/status
 import requests
 
 if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
     r = requests.get(url)
     text = r.text
     print("Body response:")
