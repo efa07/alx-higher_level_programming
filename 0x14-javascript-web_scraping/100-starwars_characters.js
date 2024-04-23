6d4c142e75a61887a019454ece03aecbf384acc3
Efa <efatariku07@gmail.com>
2024-04-23 16:16:42 +0300

    starwars characters

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,22 +1,25 @@
 #!/usr/bin/node
 
-const req = require("request");
+const request = require('request');
 const id = process.argv[2];
-const url = "https://swapi-api.hbtn.io/api/films/";
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-req.get(url + id, function (error, res, body) {
+request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
-  }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, res, body1) {
-      if (error) {
-        console.log(error);
-      }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
-    });
+  } else {
+    const content = JSON.parse(body);
+    const characters = content.characters;
+    // console.log(characters);
+    for (const character of characters) {
+      request.get(character, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        } else {
+          const names = JSON.parse(body);
+          console.log(names.name);
+        }
+      });
+    }
   }
 });
